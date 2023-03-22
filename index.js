86be9c771ed6b94ce785acf7235dd7daf5acc28b
Steve <kamaustevek78@gmail.com>
2023-03-22 10:02:15 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("JavaScript is so cool. It lets me add text to my page programmatically");
+
+const event = document.createEvent( 'Event' );
+event.initEvent( 'DOMContentLoaded', true, true );
+window.document.dispatchEvent( event );
+
+document.querySelector("p")
+console.log("This is really cool");
+})
